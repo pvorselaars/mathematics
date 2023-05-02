d1fb4882ef4842cd6bb27864c831dcc82e8039da
Perry Vorselaars <p.vorselaars@gmail.com>
2023-05-02 20:31:17 +0200

    Update EP2C

--- kinematics/EP2C.m
+++ kinematics/EP2C.m
@@ -1,16 +1,20 @@
 function C = EP2C(q)
 
+    % EP2C
+    %
+    %   C = EP2C(q) calculates the direction cosine matrix C
+    %   from the Euler parameters q. The first element of q
+    %   should be the non-dimensional Euler parameter
+
+    if !isequal(q,[4 1])
+        error('q should be a 4x1 vector')
+    end
+
     q0 = q(1);
     q1 = q(2);
     q2 = q(3);
     q3 = q(4);
 
-    C(1,1) = q0*q0+q1*q1-q2*q2-q3*q3;
-    C(1,2) = 2*(q1*q2+q0*q3);
-    C(1,3) = 2*(q1*q3-q0*q2);
-    C(2,1) = 2*(q1*q2-q0*q3);
-    C(2,2) = q0*q0-q1*q1+q2*q2-q3*q3;
-    C(2,3) = 2*(q2*q3+q0*q1);
-    C(3,1) = 2*(q1*q3 + q0*q2);
-    C(3,2) = 2*(q2*q3-q0*q1);
-    C(3,3) = q0*q0-q1*q1-q2*q2+q3*q3;
+    C = [ q0*q0+q1*q1-q2*q2-q3*q3 2*(q1*q2+q0*q3)           2*(q1*q3-q0*q2);
+          2*(q1*q2-q0*q3)         q0*q0-q1*q1+q2*q2-q3*q3   2*(q2*q3+q0*q1);
+          2*(q1*q3 + q0*q2)       2*(q2*q3-q0*q1)           q0*q0-q1*q1-q2*q2+q3*q3 ];
