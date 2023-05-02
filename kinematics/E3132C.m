aceb46a03b94114f1b6e238bd5a7cb96dc7c9795
Perry Vorselaars <p.vorselaars@gmail.com>
2023-05-02 14:00:37 +0200

    Euler 3-1-3 to DCM

---
+++ kinematics/E3132C.m
@@ -0,0 +1,20 @@
+function C = E3132C(q)
+
+	% E3132C
+	%
+	%		C = E3212C(q) computes the direction
+	%		cosine matrix for 3-1-3 Euler angles.
+	%		q must be a 3x1 vector of Euler angles.
+	%
+	
+	sq1 = sin(q(1));
+	cq1 = cos(q(1));
+	sq2 = sin(q(2));
+	cq2 = cos(q(2));
+	sq3 = sin(q(3));
+	cq3 = cos(q(3));
+
+	C   = [ cq3*cq1 - sq3*cq2*sq1		cq3*sq1 + sq3*cq2*cq1	 sq3*sq2;
+					-sq3*cq1 - cq3*cq2*sq1	-sq3*sq1 + cq3*cq2*cq1 cq3*sq2;
+					sq2*sq1									-sq2*cq1							 cq2   ];
+
