84531fc27e28e76ef3fc60ebd91469d43f166974
Perry Vorselaars <p.vorselaars@gmail.com>
2023-05-02 20:07:17 +0200

    Fix tabs

--- kinematics/C2E313.m
+++ kinematics/C2E313.m
@@ -6,9 +6,9 @@ function q = C2E313(C)
 	% 	from the 3x3 direction cosine matrix C
 
 	if !isequal(size(C),[3 3])
-		error('C is not a 3x3 matrix')
+        error('C is not a 3x3 matrix')
 	end
 
-	q = [ atan2(C(3,1),-C(3,2));
-		  acos(C(3,3));
-		  atan2(C(1,3),C(2,3)) ];
+    q = [ atan2(C(3,1),-C(3,2));
+          acos(C(3,3));
+          atan2(C(1,3),C(2,3)) ];
